bcd71ea3d99e849c99ec479d00c91aafb4b81a5d
GeniusQ <328163@qq.com>
2015-07-15 11:25:21 +0800

    加入网站权限验证模块

---
+++ Web/Permission/Checker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLIB.Web.Permission
+{
+    public class Checker:System.Web.IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(System.Web.HttpApplication context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
