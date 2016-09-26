6d799b7fee6a4e36d34965805d87cc0570903310
GeniusQ <328163@qq.com>
2016-09-26 10:34:12 +0800

    加入RestApi基础类

---
+++ QLIB/Web/RestApi/ApiHandlerBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+namespace QLIB.Web.RestApi
+{
+    /// <summary>
+    /// HttpApi的基类
+    /// </summary>
+    public abstract class ApiHandlerBase : IHttpHandler, System.Web.SessionState.IRequiresSessionState
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        protected ApiResult ret { get; private set; }
+
+        public void ProcessRequest(HttpContext context)
+        {
+
+            ret = new ApiResult();
+
+            var cmd = context.Request.QueryString["cmd"] ?? "";
+            ret.Page = Convert.ToInt32(context.Request.Form["page"] ?? "1");
+            ret.Size = Convert.ToInt32(context.Request.Form["size"] ?? "20");
+            try
+            {
+                Do(context.Request, cmd);
+            }
+            catch (Exception ex)
+            {
+                ret.Success = false;
+                ret.Message = "服务异常【"+ cmd + "】";
+                ret.Data = ex.ToString();
+            }
+
+
+            context.Response.ContentEncoding = System.Text.Encoding.UTF8;
+            context.Response.ContentType = "text/plain";
+            var json = GetJson(ret);
+            context.Response.Write(json);
+            context.Response.Flush();
+        }
+
+        public abstract void Do(HttpRequest req, string cmd);
+
+        public abstract string GetJson(object obj);
+
+    }
+
+    public static class HttpRequestEx
+    {
+        public static string GetString(this HttpRequest obj, string key)
+        {
+            return obj[key] ?? string.Empty;
+        }
+
+        public static long GetLong(this HttpRequest obj, string key)
+        {
+            return Convert.ToInt64(obj[key] ?? "0");
+        }
+        public static Int32 GetInt(this HttpRequest obj, string key)
+        {
+            return Convert.ToInt32(obj[key] ?? "0");
+        }
+        public static decimal GetMoney(this HttpRequest obj, string key)
+        {
+            return Convert.ToDecimal(obj[key] ?? "0");
+        }
+    }
+
+}
+
---
+++ QLIB/Web/RestApi/ApiResult.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLIB.Web.RestApi
+{
+    /// <summary>
+    /// http api返回数据包
+    /// </summary>
+    public class ApiResult
+    {
+        public int ErrorCode { get; set; }
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public string Message { get; set; }
+        public int TotalCount { get; set; }
+        public int Page { get; set; }
+        public int Size { get; set; }
+        
+
+        public ApiResult()
+        {
+            Success = false;
+            Message = "API未执行";
+        }
+
+        public void Complete(object data,string msg)
+        {
+            Success = true;
+            Data = data;
+            Message = msg ?? "";
+        }
+
+        public void Fail(string msg, int code=-1,object data=null)
+        {
+            Success = false;
+            if (string.IsNullOrWhiteSpace(msg))
+                throw new ApplicationException("错误原因不能为空");
+            else
+            {
+                Message = msg;
+            }
+
+            ErrorCode = code;
+            Data = data;
+        }
+    }
+}
