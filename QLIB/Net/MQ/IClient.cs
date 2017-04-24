208279ab0015b4a093a8525ac906cb219608fb7f
Q <Q@QQ.com>
2017-04-24 21:00:27 +0800

    加入消息队列接口

---
+++ QLIB/Net/MQ/IClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QLIB.Net.MQ
+{
+    /// <summary>
+    /// 消息队列接口
+    /// </summary>
+    interface IClient
+    {
+        string Connection(string ip, string host, string channel, string account, string password);
+        string Send(string msg);
+    }
+}
